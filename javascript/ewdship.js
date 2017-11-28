914295448e1574c635c635dabe23d0da29e6f3b2
elementwebdev <ivinci.design@gmail.com>
2017-11-28 13:24:19 -0800

    CodeSchool Notes
    These notes were taken from videos and .pdf's. I have coded all the content and escaped the example code in order to display properly

---
+++ javascript/ewdship.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$(document).getElementById ( "shipping" ) {
+		function totalPrice(subtotal, shipping) {
+			$(document).getElementById( "ship") {
+				function ewdShip(subtotal, shipping) {
+					var subtotal;  // shouldn't this not have a number since its called above?
+					var shipping  = 5.99;
+					var totalPrice = subtotal + shipping;
+					console.log(  );
+		} // end ship
+	} // end
---
+++ javascript/javascript-roadtrip/js/trains.js
@@ -0,0 +1,16 @@
+// 3.4
+var totalTrains = 12;
+var trainsOperational = 8;
+trainsOperational = trainsOperational + 4;
+
+// Printing to the console
+console.log("There are " + trainsOperational + " running trains.");
+
+console.log("Train #" + 1 + " is running.");
+console.log("Train #" + 2 + " is running.");
+console.log("Train #" + 3 + " is running.");
+console.log("Train #" + 4 + " is running.");
+console.log("Train #" + 5 + " is running.");
+console.log("Train #" + 6 + " is running.");
+console.log("Train #" + 7 + " is running.");
+console.log("Train #" + 8 + " is running.");
